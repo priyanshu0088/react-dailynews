8cdf26eb38fe7a70c74cec1809d680b526055ce7
priyanshu0088 <112187158+priyanshu0088@users.noreply.github.com>
2023-01-07 22:16:59 +0530

    first commit

---
+++ src/Components/News.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Newsitem from './Newsitem'
+
+export default class News extends Component {
+  render() {
+    return (
+      <div>
+        this is news component
+        <Newsitem/>
+      </div>
+    )
+  }
+}
---
+++ src/Components/Newsitem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Newsitem extends Component {
+  render() {
+    return (
+      <div>
+        tis is news item component inside news component
+      </div>
+    )
+  }
+}
