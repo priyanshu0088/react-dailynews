cf8e754f387e6e35a0fedf9b949aa6cebf8a15ea
priyanshu0088 <112187158+priyanshu0088@users.noreply.github.com>
2023-01-07 22:30:08 +0530

    first commit

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -4,7 +4,7 @@ export default class Newsitem extends Component {
   render() {
     return (
       <div>
-        tis is news item component inside news component
+        this is news item component inside news component
       </div>
     )
   }
