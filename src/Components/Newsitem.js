750d2211972e598e3e74488160d845f89f1d867d
priyanshu0088 <112187158+priyanshu0088@users.noreply.github.com>
2023-01-10 23:47:57 +0530

    middle

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
 
 export default class Newsitem extends Component {
+
+
+
+
   render() {
+    let {title,description,imageurl,newsUrl} =this.props;
     return (
-      <div>
-        this is news item component inside news component.
+      <div className='my-3'>
+        <div className="card" style = {{width : "18rem"}}>
+  <img src={imageurl} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{title}...</h5>
+    <p className="card-text">{description}...</p>
+    <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">read more</a>
+  </div>
+</div>
       </div>
     )
   }
