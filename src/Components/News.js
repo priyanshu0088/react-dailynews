8e2623c6a1b9cc9dfd5a77d9aa87c0a918e0f9c9
priyanshu0088 <112187158+priyanshu0088@users.noreply.github.com>
2023-01-12 02:48:37 +0530

    fetch

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,33 +2,33 @@ import React, { Component } from 'react'
 import Newsitem from './Newsitem'
 
 export default class News extends Component {
-  articles = [
-    {
-      "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-      "author": null,
-      "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      "publishedAt": "2020-04-27T11:41:47Z",
-      "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-    },
-    {
-      "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-      "author": null,
-      "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      "publishedAt": "2020-03-30T15:26:05Z",
-      "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-    }
-  ]
+  // articles = [
+  //   {
+  //     "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
+  //     "author": null,
+  //     "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+  //     "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+  //     "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+  //     "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+  //     "publishedAt": "2020-04-27T11:41:47Z",
+  //     "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+  //   },
+  //   {
+  //     "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
+  //     "author": null,
+  //     "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+  //     "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+  //     "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+  //     "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+  //     "publishedAt": "2020-03-30T15:26:05Z",
+  //     "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+  //   }
+  // ]
   constructor(){
     super();
-    console.log("hellow i am ma constructor from news component")
-    this.state ={
-      articles: this.articles,
+   
+    this.state = {
+      Articles: this.Articles,
       loading: false
 
     }
@@ -40,7 +40,8 @@ async componentDidMount(){
   let data = await fetch(url);
   let parsedData = await data.json()
   console.log(parsedData);
-  this.setState({articles: parsedData.articles})
+ this.setState({Articles: parsedData.Articles})
+
 }
 
   render() {
@@ -48,7 +49,8 @@ async componentDidMount(){
       <div className='container my-3'>
         <h1>daily-news top headlines</h1>
         <div className="row">
-          {this.articles.map((element)=>{
+          {this.Articles && this.Articles.map((element)=>{
+       
             return <div className="col-md-4" key={element.url}>
             <Newsitem title={element.title} description={element.description.slice(0,88)} imageurl={element.urlToImage} newsUrl={element.url}/>
             </div>
